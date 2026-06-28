ff5f25a9f1f5222e185af7939ed0657b5af6d158
sin1111yi <sin1111yi@foxmail.com>
2026-06-28 20:15:18 +0800

    feat(event): add CompositeEvent for OR/AND multi-event combination
    - CompositeEvent holds an array of sub-events
    - OR mode: triggered when any sub-event is triggered
    - AND mode: triggered when all sub-events are triggered
    - clear() delegates to all sub-events
    
    Model: deepseek-v4-flash DeepSeek
    Agent: opencode

---
+++ src/drivers/event/CompositeEvent.h
@@ -0,0 +1,62 @@
+/**
+ * This file is a part of ThetaGP.
+ *
+ * ThetaGP is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * ThetaGP is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.
+ *
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "drivers/event/event.h"
+
+namespace ThetaGP::Drivers::Event {
+
+class CompositeEvent : public Event {
+public:
+  enum Mode { OR, AND };
+
+  CompositeEvent(Event **events, uint8_t count, Mode mode = OR)
+      : _events(events), _count(count), _mode(mode) {}
+
+  bool isTriggered() override {
+    if (_mode == OR) {
+      for (uint8_t i = 0; i < _count; i++) {
+        if (_events[i]->isTriggered())
+          return true;
+      }
+      return false;
+    }
+    for (uint8_t i = 0; i < _count; i++) {
+      if (!_events[i]->isTriggered())
+        return false;
+    }
+    return true;
+  }
+
+  void clear() override {
+    for (uint8_t i = 0; i < _count; i++) {
+      _events[i]->clear();
+    }
+  }
+
+private:
+  Event **_events;
+  uint8_t _count;
+  Mode _mode;
+};
+
+} // namespace ThetaGP::Drivers::Event
